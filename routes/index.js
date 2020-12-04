f537cb38894e1ff2d1028fbb5b261fe55ee703b3
Areg <aregghaz@gmail.com>
2020-12-04 11:26:06 +0400

    test

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const Users = require('../models/users.js');
-const {check, validationResult, body} = require("express-validator");
-var bcrypt = require("bcryptjs");
-var fs = require("fs");
-const saltRounds = 10;
 
-
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
   res.render('auth', { title: 'login | test project' });
 });
-
-
-
+/* GET registration page. */
 router.get('/registration', function(req, res, next) {
   res.render('reg', { title: 'login | create new user',error :false });
 });
-
+/* GET recovery page. */
 router.get('/recovery', function(req, res, next) {
   res.render('recovery', { title: 'recovery | recovery password' });
 });
-
 
 module.exports = router;
